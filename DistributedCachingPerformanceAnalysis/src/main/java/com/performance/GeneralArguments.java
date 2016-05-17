c80a7d91750b6ca3c46e495ab90f0866a348018b
Haytham Salhi <hsalhi89@gmail.com>
2016-05-17 02:15:04 +0200

    Increasing the size of cache to 20000

--- DistributedCachingPerformanceAnalysis/src/main/java/com/performance/GeneralArguments.java
+++ DistributedCachingPerformanceAnalysis/src/main/java/com/performance/GeneralArguments.java
@@ -2,6 +2,6 @@ package com.performance;
 
 public class GeneralArguments {
 	public static String serverIP = "10.0.0.1";
-	public static int cacheSize = 5000;
+	public static int cacheSize = 20000;
 	public static int agesBound = 40;
 }
