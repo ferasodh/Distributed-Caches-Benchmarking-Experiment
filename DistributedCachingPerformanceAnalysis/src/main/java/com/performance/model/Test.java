2b932f067d1cbf20ea625cdc31e63c0aac56b8c5
test1 <test1@test1-HP-EliteDesk-800-G1-TWR>
2016-06-14 17:52:48 +0300

    -Get max employee and min employee

--- DistributedCachingPerformanceAnalysis/src/main/java/com/performance/model/Test.java
+++ DistributedCachingPerformanceAnalysis/src/main/java/com/performance/model/Test.java
@@ -11,14 +11,16 @@ public class Test {
         List <Employee> list = testt.getEmployeesFirst(1000000);
         System.out.println(list.get(1).toString());
         System.out.println(list.get(5).toString());
-        System.out.println(list.get(10).toString());
+        System.out.println(list.get(999999).toString());
         System.out.println("\n\n===============Testing get List Of Organizations By max index===============");
         List <Organization> list2 = testt.getOrganizationsFirst(1000);
         System.out.println(list2.get(1).toString());
         System.out.println(list2.get(5).toString());
-        System.out.println(list2.get(9).toString());
+        System.out.println(list2.get(999).toString());
         System.out.println("\n\n===============Testing get Employee By index===============");
         System.out.println(testt.getEmployee(1));
+        System.out.println(testt.getEmployee(999999));
+        
         System.out.println("\n\n===============Testing get Organization By index===============");
         System.out.println(testt.getOrganization(5));
     }
